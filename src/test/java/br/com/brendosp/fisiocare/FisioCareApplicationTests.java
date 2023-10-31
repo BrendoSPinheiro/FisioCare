c638ef9319ab235ab02ceba75c63d2a2b96abdf0
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2023-10-31 09:23:19 -0300

    chore: init project

---
+++ src/test/java/br/com/brendosp/fisiocare/FisioCareApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brendosp.fisiocare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisioCareApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
