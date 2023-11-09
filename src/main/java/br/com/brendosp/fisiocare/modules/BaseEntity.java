f38c9650b62bb4d2032b8dc398b901d71f76798a
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2023-11-09 09:33:15 -0300

    chore: create BaseEntity

---
+++ src/main/java/br/com/brendosp/fisiocare/modules/BaseEntity.java
@@ -0,0 +1,25 @@
+package br.com.brendosp.fisiocare.modules;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+public abstract class BaseEntity {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @CreationTimestamp
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  @Column(name = "updated_at", nullable = false)
+  private LocalDateTime updatedAt;
+}
